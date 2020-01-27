1f15ad86e7b7ebcd430459b19889ca67804f0a8e
jwross24 <jwross24@users.noreply.github.com>
2020-01-27 17:13:31 -0500

    init app

---
+++ twitoff-src/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        return "Index Page"
+
+    @app.route('/hello')
+    def hello():
+        return "Hello, world!"
+
+    return app
