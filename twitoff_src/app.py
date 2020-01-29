a1744e26a2449755653185168ba15ee8d9b7257b
jwross24 <jwross24@users.noreply.github.com>
2020-01-29 09:30:52 -0500

    delete tweet generator, add db reset

--- twitoff_src/app.py
+++ twitoff_src/app.py
@@ -1,7 +1,6 @@
 from os import getenv
-import uuid
 from flask import Flask, render_template
-from .models import DB, User, Tweet
+from .models import DB, User
 
 
 def create_app():
@@ -16,18 +15,10 @@ def create_app():
         users = User.query.all()
         return render_template('base.html', title='Index Page', users=users)
 
-    @app.route('/hello')
-    def hello():
-        rand_name = str(uuid.uuid4())
-        rand_u = User(name=rand_name)
-
-        for i in range(3):
-            new_tweet = Tweet(text=f'Tweet #{i}')
-            rand_u.tweets.append(new_tweet)
-
-        DB.session.add(rand_u)
-        DB.session.commit()
-
-        return render_template('base.html', title='hello')
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title="DB Reset!", users=[])
 
     return app
