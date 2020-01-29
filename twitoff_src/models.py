9eb2fd6564e838beb7ff241918280ba6a58f7fce
jwross24 <jwross24@users.noreply.github.com>
2020-01-29 09:30:30 -0500

    modify models for new fields

--- twitoff_src/models.py
+++ twitoff_src/models.py
@@ -7,6 +7,7 @@ class User(DB.Model):
     """Twitter users that we pull and analyze Tweets for."""
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(20), nullable=False)
+    newest_tweet_id = DB.Column(DB.BigInteger)
 
     def __repr__(self):
         return f'<User {self.name}>'
@@ -14,8 +15,9 @@ class User(DB.Model):
 
 class Tweet(DB.Model):
     """Tweets."""
-    id = DB.Column(DB.Integer, primary_key=True)
-    text = DB.Column(DB.Unicode(280), nullable=False)
+    id = DB.Column(DB.BigInteger, primary_key=True)
+    text = DB.Column(DB.Unicode(500), nullable=False)
+    embedding = DB.Column(DB.PickleType, nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
