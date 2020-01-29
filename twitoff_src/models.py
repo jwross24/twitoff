6bdbbae627579c1567c90cb074bf8e57e781dfdb
jwross24 <jwross24@users.noreply.github.com>
2020-01-29 08:08:36 -0500

    add repr to models

--- twitoff_src/models.py
+++ twitoff_src/models.py
@@ -4,12 +4,20 @@ DB = SQLAlchemy()
 
 
 class User(DB.Model):
+    """Twitter users that we pull and analyze Tweets for."""
     id = DB.Column(DB.Integer, primary_key=True)
     name = DB.Column(DB.String(20), nullable=False)
 
+    def __repr__(self):
+        return f'<User {self.name}>'
+
 
 class Tweet(DB.Model):
+    """Tweets."""
     id = DB.Column(DB.Integer, primary_key=True)
     text = DB.Column(DB.Unicode(280), nullable=False)
     user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
+
+    def __repr__(self):
+        return f'<Tweet {self.text}>'
