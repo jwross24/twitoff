a058fead11a58fb780e44d2bb53f356bed8b1a47
jwross24 <jwross24@users.noreply.github.com>
2020-01-29 10:12:22 -0500

    fix models

--- twitoff_src/models.py
+++ twitoff_src/models.py
@@ -18,7 +18,8 @@ class Tweet(DB.Model):
     id = DB.Column(DB.BigInteger, primary_key=True)
     text = DB.Column(DB.Unicode(500), nullable=False)
     embedding = DB.Column(DB.PickleType, nullable=False)
-    user_id = DB.Column(DB.Integer, DB.ForeignKey('user.id'), nullable=False)
+    user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'),
+                        nullable=False)
     user = DB.relationship('User', backref=DB.backref('tweets', lazy=True))
 
     def __repr__(self):
