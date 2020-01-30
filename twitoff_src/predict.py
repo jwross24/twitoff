42280c9713002ff775eca135fd9f14813bd7bc92
jwross24 <jwross24@users.noreply.github.com>
2020-01-30 12:32:29 -0500

    predict using KNN

--- twitoff_src/predict.py
+++ twitoff_src/predict.py
@@ -1,7 +1,7 @@
 """Prediction of users based on Tweet embeddings."""
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-
+# from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 from .models import User
 from .twitter import BASILICA
 
@@ -16,7 +16,8 @@ def predict_user(user1_name, user2_name, tweet_text):
     embeddings = np.vstack([user1_embeddings, user2_embeddings])
     labels = np.concatenate([np.ones(len(user1.tweets)),
                              np.zeros(len(user2.tweets))])
-    log_reg = LogisticRegression(max_iter=1000).fit(embeddings, labels)
+    knnc = KNeighborsClassifier(weight='distance', metric='cosine').fit(
+        embeddings, labels)
 
     tweet_embedding = BASILICA.embed_sentence(tweet_text, model='twitter')
-    return log_reg.predict(np.array(tweet_embedding).reshape(1, -1))
+    return knnc.predict(np.array(tweet_embedding).reshape(1, -1))
